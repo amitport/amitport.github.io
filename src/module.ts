951501fc3703b11789c32713521b507d74165d79
Amit Portnoy <amit.portnoy@gmail.com>
2017-10-10 20:25:45 +0300

    Auto-generated commit

--- src/module.ts
+++
@@ -1,6 +0,0 @@
-import {module} from 'angular';
-import 'angular-material';
-import shortNumber from 'short-number';
-
-export default module('amitport', ['ngMaterial'])
-.filter('shortNumber', () => shortNumber);
