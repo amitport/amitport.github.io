0e949dbfea13bfded268517bbd5b9d34e410b87b
Amit Portnoy <1131991+amitport@users.noreply.github.com>
2019-03-03 13:47:26 +0200

    avoid Angular's icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProjectListComponent } from './project-list/project-list.component';
@@ -15,7 +13,6 @@ import { FlexLayoutModule } from '@angular/flex-layout';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
 
     FlexLayoutModule,
